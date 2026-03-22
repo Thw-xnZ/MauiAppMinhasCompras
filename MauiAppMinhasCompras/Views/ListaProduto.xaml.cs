899830075edd5c1aaa2237e08402a94207f56b37
uTêe ` <pabloestevamgm@gmail.com>
2026-03-22 08:56:12 -0300

    Finalização do Basico do aplicativo (remover e selecionar itens da lista)

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,5 +1,6 @@
 using MauiAppMinhasCompras.Models;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace MauiAppMinhasCompras.Views;
 
@@ -19,9 +20,15 @@ public partial class ListaProduto : ContentPage
     // mantendo a pagina sempre atualizada quando ela é recarregada
     protected async override void OnAppearing()
     {
+        try { 
 		List<Produto> tmp = await App.Db.GetAll();
 
 		tmp.ForEach(i => lista.Add(i));
+    }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", $"Ocorreu um erro: {ex.Message}", "OK");
+        }
     }
 
     // Botão que foi criado para a utilização do ToolbarItem, para navegar para a página de cadastro de produto
@@ -30,9 +37,10 @@ public partial class ListaProduto : ContentPage
 		try
 		{
 			Navigation.PushAsync(new Views.NovoProduto());
-        } catch (Exception ex)
+        } 
+        catch (Exception ex)
 		{
-			DisplayAlert("Ops", "Ocorreu um erro: {ex.Message}", "OK");
+			DisplayAlert("Ops", $"Ocorreu um erro: {ex.Message}", "OK");
 			return;
         }
     }
@@ -41,6 +49,7 @@ public partial class ListaProduto : ContentPage
     // permitindo que a lista de produtos seja atualizada dinamicamente com base na consulta de busca fornecida pelo usuário.
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
+        try { 
 		string q = e.NewTextValue;
 
 		lista.Clear();
@@ -48,21 +57,64 @@ public partial class ListaProduto : ContentPage
         List<Produto> tmp = await App.Db.Search(q);
 
 		tmp.ForEach(i => lista.Add(i)); 
+        }  
+        catch (Exception ex)
+            {
+                await DisplayAlert("Ops", $"Ocorreu um erro: {ex.Message}", "OK");
+            }
     }
 
     // Aqui é o botão para calcular o valor total dos produtos.
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
     {
+        try { 
 		double soma = lista.Sum(i => i.Total);
 
 		string msg = $"O valor total é: {soma:C}";
 
 		DisplayAlert("Total dos Produtos", msg, "Ok");
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", $"Ocorreu um erro: {ex.Message}", "OK");
+        }
     }
 
     // Projeto de exclusão de um produto, onde o usuário pode clicar no item da lista e escolher a opção de excluir o produto selecionado.
-    private void MenuItem_Clicked(object sender, EventArgs e)
+    private async Task MenuItem_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            MenuItem selecionado = sender as MenuItem;
 
+            Produto p = selecionado.BindingContext as Produto;
+
+            bool confirm = await DisplayAlert("Confirmação", $"Deseja excluir o produto {p.Descricao}?", "Sim", "Não");
+
+            if (confirm)
+            {
+                await App.Db.Delete(p.Id);
+                lista.Remove(p);
+            }
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", $"Ocorreu um erro: {ex.Message}", "OK");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        try
+        {
+            Produto p = e.SelectedItem as Produto; 
+
+            Navigation.PushAsync(new Views.EditarProduto { BindingContext = p });
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", $"Ocorreu um erro: {ex.Message}", "OK");
+        }
     }
 }
